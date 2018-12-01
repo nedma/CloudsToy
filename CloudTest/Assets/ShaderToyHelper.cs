3fa4c96e3fb4d0a62b7a2bcfc12e1f31d1d850f9
nedma <realned_07@163.com>
2018-12-01 21:57:23 +0800

    * add

--- CloudTest/Assets/ShaderToyHelper.cs
+++ CloudTest/Assets/ShaderToyHelper.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 using System.Collections;
 
-public class ShaderToyHelper : MonoBehaviour {
+public class ShaderToyHelper : MonoBehaviour
+{
 
     private Material _material = null;
 
     private bool _isDragging = false;
 
     // Use this for initialization
-    void Start () {
+    void Start ()
+    {
         Renderer render  = GetComponent<Renderer>();
-        if (render != null) {
+        if (render != null)
+        {
             _material = render.material;
         }
 
@@ -18,24 +21,31 @@ public class ShaderToyHelper : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update ()
+    {
         Vector3 mousePosition = Vector3.zero;
-        if (_isDragging) {
+        if (_isDragging)
+        {
             mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.0f);
-        } else {
+        }
+        else
+        {
             mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f);
         }
 
-        if (_material != null) {
+        if (_material != null)
+        {
             _material.SetVector("iMouse", mousePosition);
         }
     }
 
-    void OnMouseDown() {
+    void OnMouseDown()
+    {
         _isDragging = true;
     }
 
-    void OnMouseUp() {
+    void OnMouseUp()
+    {
         _isDragging = false;
     }
 }
